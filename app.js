df4903f9a59514434ecb8909366196323670789f
Santhosh Kumar <san.devjs@gmail.com>
2017-11-30 16:40:10 -0500

    CI CD

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var app = express();
 //testing jenkin slave 2
 
 app.get('/',function(req,res) {
-  res.send("<h1> This should work now </h1>");
+  res.send("<h1>  Showcasing CI/CD to pedro </h1>");
 });
 
 app.listen(4000);
