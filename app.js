3f6a54fc62d11eb3346128716ca100af37818442
Santhosh Kumar <san.devjs@gmail.com>
2017-11-30 16:02:49 -0500

    both server checking

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var app = express();
 //testing jenkin slave 2
 
 app.get('/',function(req,res) {
-  res.send("<h1>Hello world This thing is working good</h1>");
+  res.send("<h1>Checking the both serversd</h1>");
 });
 
 app.listen(4000);
