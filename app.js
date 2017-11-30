223f164ca7630ec1ac2753b95ee83b9e16cd26ce
Santhosh Kumar <san.devjs@gmail.com>
2017-11-30 16:24:38 -0500

    This should work ?

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var app = express();
 //testing jenkin slave 2
 
 app.get('/',function(req,res) {
-  res.send("<h1>Checking the both serversd</h1>");
+  res.send("<h1> This should work now </h1>");
 });
 
 app.listen(4000);
